e8184a0d8fdaad4a21e421fe6a9a1c3ae18ada88
Darrin Spell Jr <dspell74@gmail.com>
2018-10-20 21:55:41 -0700

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -18,30 +18,37 @@ def start_game():
         print("Have A Nice Day {}!".format(first_name))
         sys.exit()
     else:
-        print("Welcome {} to the Number Guessing Game!".format(first_name))
+        print("\nWelcome {} to the Number Guessing Game!".format(first_name))
         print("Guess A Number From 1-100!")
 
 while user_input != random_number:
     try:
         user_input = int(input())
         attempt_count += 1
-        score_counter += 25
+            score_counter += 25
         except ValueError:
             print("Oh no thats not a valid answer! Guess again!")
-        except TypeError:
-            print("Oh no thats not a valid answer! Guess again!")
+    except TypeError:
+        print("Oh no thats not a valid answer! Guess again!")
         else:
             if user_input == random_number:
                 print("Congratulations You Guessed It!")
                 print("It took you {} attempts and you scored {} points!".format(attempt_count, score_counter))
                 ending = input("Thank You For Playing The Number Guessing Game! Would you like to play again? Y/N  ")
                 attempt_count == score_counter
+                if attempt_count == 0:
+                    print("\nThis is your first time playing the Number Guessing Game. So there is no high score.")
+                else:
+                    print("\nCurrent best score is {}!".format(score_counter))
+                    attempt_count == score_counter
                 if ending.lower() == "n":
                     print("Have A Nice Day {}!".format(first_name))
                     sys.exit()
+                    continue
                 if ending.lower() == "no":
                     print("Have A Nice Day {}!".format(first_name))
                     sys.exit()
+                    continue
                 if ending.lower() == "":
                     continue
                 start_game()
@@ -66,4 +73,3 @@ user_input == random_number
 
 if __name__ == '__main__':
     start_game()
-
