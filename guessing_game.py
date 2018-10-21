4e04a112f09f0490d907257589ea19bf85b3b255
Darrin Spell Jr <dspell74@gmail.com>
2018-10-20 22:10:28 -0700

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -7,29 +7,30 @@ def start_game():
     attempt_count = 0
     score_counter = 0
     random_number = int(random.randrange(1,100))
-    
+
     first_name = input("What is your name?  ")
     answer = input("Do you want to play the Number Guessing Game? Y/N  ")
-    
+
     if answer.lower() == "n":
-        print("Have A Nice Day {}!".format(first_name))
-        sys.exit()
+          print("Have A Nice Day {}!".format(first_name))
+          sys.exit()
     elif answer.lower() == "no":
-        print("Have A Nice Day {}!".format(first_name))
-        sys.exit()
+          print("Have A Nice Day {}!".format(first_name))
+          sys.exit()   
     else:
         print("\nWelcome {} to the Number Guessing Game!".format(first_name))
+        print("The lower the score the better!")
         print("Guess A Number From 1-100!")
 
-while user_input != random_number:
-    try:
-        user_input = int(input())
-        attempt_count += 1
+    while user_input != random_number:
+        try:
+            user_input = int(input())
+            attempt_count += 1
             score_counter += 25
         except ValueError:
             print("Oh no thats not a valid answer! Guess again!")
-    except TypeError:
-        print("Oh no thats not a valid answer! Guess again!")
+        except TypeError:
+            print("Oh no thats not a valid answer! Guess again!")
         else:
             if user_input == random_number:
                 print("Congratulations You Guessed It!")
@@ -63,13 +64,14 @@ while user_input != random_number:
             else:
                 if attempt_count != '1':
                     print("Congratulations! You Got It On Your First Try!!!")
+                
+    user_input == random_number
 
-user_input == random_number
-
-
+        
 
 
 
 
 if __name__ == '__main__':
     start_game()
+   
